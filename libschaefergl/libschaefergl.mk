1ce0125f701c0a9ba2c1a109eb25be5b1a3de204
Christopher Joseph Dean Schaefer <disks86@gmail.com>
2014-08-02 13:07:04 -0400

    missed last commit.

--- libschaefergl/libschaefergl.mk
+++ libschaefergl/libschaefergl.mk
@@ -13,7 +13,7 @@ CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
 User                   :=Christopher Joseph Dean Schaefer
-Date                   :=07/29/2014
+Date                   :=08/02/2014
 CodeLitePath           :="/home/cschaefer/.codelite"
 LinkerName             :=g++
 SharedObjectLinkerName :=g++ -shared -fPIC
@@ -58,7 +58,7 @@ CFLAGS   :=  -g $(Preprocessors)
 ## User defined environment variables
 ##
 CodeLiteDir:=/usr/share/codelite
-Objects=$(IntermediateDirectory)/i_unknown$(ObjectSuffix) 
+Objects=$(IntermediateDirectory)/i_unknown$(ObjectSuffix) $(IntermediateDirectory)/i_direct3d_device9$(ObjectSuffix) $(IntermediateDirectory)/i_direct3d_resource9$(ObjectSuffix) $(IntermediateDirectory)/i_direct3d_surface9$(ObjectSuffix) 
 
 ##
 ## Main Build Targets 
@@ -91,6 +91,30 @@ $(IntermediateDirectory)/i_unknown$(DependSuffix): i_unknown.cpp
 $(IntermediateDirectory)/i_unknown$(PreprocessSuffix): i_unknown.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/i_unknown$(PreprocessSuffix) "/home/cschaefer/Documents/SchaeferGL/libschaefergl/i_unknown.cpp"
 
+$(IntermediateDirectory)/i_direct3d_device9$(ObjectSuffix): i_direct3d_device9.cpp $(IntermediateDirectory)/i_direct3d_device9$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/cschaefer/Documents/SchaeferGL/libschaefergl/i_direct3d_device9.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/i_direct3d_device9$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/i_direct3d_device9$(DependSuffix): i_direct3d_device9.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/i_direct3d_device9$(ObjectSuffix) -MF$(IntermediateDirectory)/i_direct3d_device9$(DependSuffix) -MM "/home/cschaefer/Documents/SchaeferGL/libschaefergl/i_direct3d_device9.cpp"
+
+$(IntermediateDirectory)/i_direct3d_device9$(PreprocessSuffix): i_direct3d_device9.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/i_direct3d_device9$(PreprocessSuffix) "/home/cschaefer/Documents/SchaeferGL/libschaefergl/i_direct3d_device9.cpp"
+
+$(IntermediateDirectory)/i_direct3d_resource9$(ObjectSuffix): i_direct3d_resource9.cpp $(IntermediateDirectory)/i_direct3d_resource9$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/cschaefer/Documents/SchaeferGL/libschaefergl/i_direct3d_resource9.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/i_direct3d_resource9$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/i_direct3d_resource9$(DependSuffix): i_direct3d_resource9.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/i_direct3d_resource9$(ObjectSuffix) -MF$(IntermediateDirectory)/i_direct3d_resource9$(DependSuffix) -MM "/home/cschaefer/Documents/SchaeferGL/libschaefergl/i_direct3d_resource9.cpp"
+
+$(IntermediateDirectory)/i_direct3d_resource9$(PreprocessSuffix): i_direct3d_resource9.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/i_direct3d_resource9$(PreprocessSuffix) "/home/cschaefer/Documents/SchaeferGL/libschaefergl/i_direct3d_resource9.cpp"
+
+$(IntermediateDirectory)/i_direct3d_surface9$(ObjectSuffix): i_direct3d_surface9.cpp $(IntermediateDirectory)/i_direct3d_surface9$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/cschaefer/Documents/SchaeferGL/libschaefergl/i_direct3d_surface9.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/i_direct3d_surface9$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/i_direct3d_surface9$(DependSuffix): i_direct3d_surface9.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/i_direct3d_surface9$(ObjectSuffix) -MF$(IntermediateDirectory)/i_direct3d_surface9$(DependSuffix) -MM "/home/cschaefer/Documents/SchaeferGL/libschaefergl/i_direct3d_surface9.cpp"
+
+$(IntermediateDirectory)/i_direct3d_surface9$(PreprocessSuffix): i_direct3d_surface9.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/i_direct3d_surface9$(PreprocessSuffix) "/home/cschaefer/Documents/SchaeferGL/libschaefergl/i_direct3d_surface9.cpp"
+
 
 -include $(IntermediateDirectory)/*$(DependSuffix)
 ##
@@ -100,6 +124,15 @@ clean:
 	$(RM) $(IntermediateDirectory)/i_unknown$(ObjectSuffix)
 	$(RM) $(IntermediateDirectory)/i_unknown$(DependSuffix)
 	$(RM) $(IntermediateDirectory)/i_unknown$(PreprocessSuffix)
+	$(RM) $(IntermediateDirectory)/i_direct3d_device9$(ObjectSuffix)
+	$(RM) $(IntermediateDirectory)/i_direct3d_device9$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/i_direct3d_device9$(PreprocessSuffix)
+	$(RM) $(IntermediateDirectory)/i_direct3d_resource9$(ObjectSuffix)
+	$(RM) $(IntermediateDirectory)/i_direct3d_resource9$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/i_direct3d_resource9$(PreprocessSuffix)
+	$(RM) $(IntermediateDirectory)/i_direct3d_surface9$(ObjectSuffix)
+	$(RM) $(IntermediateDirectory)/i_direct3d_surface9$(DependSuffix)
+	$(RM) $(IntermediateDirectory)/i_direct3d_surface9$(PreprocessSuffix)
 	$(RM) $(OutputFile)
 	$(RM) "/home/cschaefer/Documents/SchaeferGL/.build-debug/libschaefergl"
 
--- libschaefergl/types.h
+++ libschaefergl/types.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2014 Christopher Joseph Dean Schaefer
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+ 
 #ifndef TYPES_H
 #define TYPES_H
 
@@ -14,8 +32,158 @@ typedef struct _GUID
 
 typedef unsigned long ULONG;
 typedef signed long LONG;
+typedef unsigned int UINT;
+typedef int INT;
+typedef unsigned char BYTE;
+
 typedef LONG HRESULT;
 typedef GUID IID;
 typedef IID *REFIID;
+typedef unsigned long DWORD;
+typedef DWORD D3DCOLOR;
+typedef void *PVOID;
+typedef PVOID HANDLE;
+typedef HANDLE HDC;
+
+#define MAKEFOURCC(c0,c1,c2,c3) ((DWORD)(BYTE)(c0)|((DWORD)(BYTE)(c1)<<8)|((DWORD)(BYTE)(c2)<<16)|((DWORD)(BYTE)(c3)<<24))
+
+typedef struct _RECT {
+  LONG left;
+  LONG top;
+  LONG right;
+  LONG bottom;
+} RECT, *PRECT;
+
+typedef struct D3DRECT {
+  LONG x1;
+  LONG y1;
+  LONG x2;
+  LONG y2;
+} D3DRECT;
+
+typedef struct D3DLOCKED_RECT {
+  INT  Pitch;
+  void *pBits;
+} D3DLOCKED_RECT, *LPD3DLOCKED_RECT;
+
+typedef enum D3DRESOURCETYPE { 
+  D3DRTYPE_SURFACE        = 1,
+  D3DRTYPE_VOLUME         = 2,
+  D3DRTYPE_TEXTURE        = 3,
+  D3DRTYPE_VOLUMETEXTURE  = 4,
+  D3DRTYPE_CubeTexture    = 5,
+  D3DRTYPE_VERTEXBUFFER   = 6,
+  D3DRTYPE_INDEXBUFFER    = 7,
+  D3DRTYPE_FORCE_DWORD    = 0x7fffffff
+} D3DRESOURCETYPE, *LPD3DRESOURCETYPE;
+
+typedef enum _D3DFORMAT {
+    D3DFMT_UNKNOWN              =  0,
+    D3DFMT_R8G8B8               = 20,
+    D3DFMT_A8R8G8B8             = 21,
+    D3DFMT_X8R8G8B8             = 22,
+    D3DFMT_R5G6B5               = 23,
+    D3DFMT_X1R5G5B5             = 24,
+    D3DFMT_A1R5G5B5             = 25,
+    D3DFMT_A4R4G4B4             = 26,
+    D3DFMT_R3G3B2               = 27,
+    D3DFMT_A8                   = 28,
+    D3DFMT_A8R3G3B2             = 29,
+    D3DFMT_X4R4G4B4             = 30,
+    D3DFMT_A2B10G10R10          = 31,
+    D3DFMT_A8B8G8R8             = 32,
+    D3DFMT_X8B8G8R8             = 33,
+    D3DFMT_G16R16               = 34,
+    D3DFMT_A2R10G10B10          = 35,
+    D3DFMT_A16B16G16R16         = 36,
+    D3DFMT_A8P8                 = 40,
+    D3DFMT_P8                   = 41,
+    D3DFMT_L8                   = 50,
+    D3DFMT_A8L8                 = 51,
+    D3DFMT_A4L4                 = 52,
+    D3DFMT_V8U8                 = 60,
+    D3DFMT_L6V5U5               = 61,
+    D3DFMT_X8L8V8U8             = 62,
+    D3DFMT_Q8W8V8U8             = 63,
+    D3DFMT_V16U16               = 64,
+    D3DFMT_A2W10V10U10          = 67,
+    D3DFMT_UYVY                 = MAKEFOURCC('U', 'Y', 'V', 'Y'),
+    D3DFMT_R8G8_B8G8            = MAKEFOURCC('R', 'G', 'B', 'G'),
+    D3DFMT_YUY2                 = MAKEFOURCC('Y', 'U', 'Y', '2'),
+    D3DFMT_G8R8_G8B8            = MAKEFOURCC('G', 'R', 'G', 'B'),
+    D3DFMT_DXT1                 = MAKEFOURCC('D', 'X', 'T', '1'),
+    D3DFMT_DXT2                 = MAKEFOURCC('D', 'X', 'T', '2'),
+    D3DFMT_DXT3                 = MAKEFOURCC('D', 'X', 'T', '3'),
+    D3DFMT_DXT4                 = MAKEFOURCC('D', 'X', 'T', '4'),
+    D3DFMT_DXT5                 = MAKEFOURCC('D', 'X', 'T', '5'),
+    D3DFMT_D16_LOCKABLE         = 70,
+    D3DFMT_D32                  = 71,
+    D3DFMT_D15S1                = 73,
+    D3DFMT_D24S8                = 75,
+    D3DFMT_D24X8                = 77,
+    D3DFMT_D24X4S4              = 79,
+    D3DFMT_D16                  = 80,
+    D3DFMT_D32F_LOCKABLE        = 82,
+    D3DFMT_D24FS8               = 83,
+    D3DFMT_D32_LOCKABLE         = 84,
+    D3DFMT_S8_LOCKABLE          = 85,
+    D3DFMT_L16                  = 81,
+    D3DFMT_VERTEXDATA           =100,
+    D3DFMT_INDEX16              =101,
+    D3DFMT_INDEX32              =102,
+    D3DFMT_Q16W16V16U16         =110,
+    D3DFMT_MULTI2_ARGB8         = MAKEFOURCC('M','E','T','1'),
+    D3DFMT_R16F                 = 111,
+    D3DFMT_G16R16F              = 112,
+    D3DFMT_A16B16G16R16F        = 113,
+    D3DFMT_R32F                 = 114,
+    D3DFMT_G32R32F              = 115,
+    D3DFMT_A32B32G32R32F        = 116,
+    D3DFMT_CxV8U8               = 117,
+    D3DFMT_A1                   = 118,
+    D3DFMT_A2B10G10R10_XR_BIAS  = 119,
+    D3DFMT_BINARYBUFFER         = 199,
+    D3DFMT_FORCE_DWORD          =0x7fffffff
+} D3DFORMAT;
+
+typedef enum D3DPOOL { 
+  D3DPOOL_DEFAULT      = 0,
+  D3DPOOL_MANAGED      = 1,
+  D3DPOOL_SYSTEMMEM    = 2,
+  D3DPOOL_SCRATCH      = 3,
+  D3DPOOL_FORCE_DWORD  = 0x7fffffff
+} D3DPOOL, *LPD3DPOOL;
+
+typedef enum D3DMULTISAMPLE_TYPE { 
+  D3DMULTISAMPLE_NONE          = 0,
+  D3DMULTISAMPLE_NONMASKABLE   = 1,
+  D3DMULTISAMPLE_2_SAMPLES     = 2,
+  D3DMULTISAMPLE_3_SAMPLES     = 3,
+  D3DMULTISAMPLE_4_SAMPLES     = 4,
+  D3DMULTISAMPLE_5_SAMPLES     = 5,
+  D3DMULTISAMPLE_6_SAMPLES     = 6,
+  D3DMULTISAMPLE_7_SAMPLES     = 7,
+  D3DMULTISAMPLE_8_SAMPLES     = 8,
+  D3DMULTISAMPLE_9_SAMPLES     = 9,
+  D3DMULTISAMPLE_10_SAMPLES    = 10,
+  D3DMULTISAMPLE_11_SAMPLES    = 11,
+  D3DMULTISAMPLE_12_SAMPLES    = 12,
+  D3DMULTISAMPLE_13_SAMPLES    = 13,
+  D3DMULTISAMPLE_14_SAMPLES    = 14,
+  D3DMULTISAMPLE_15_SAMPLES    = 15,
+  D3DMULTISAMPLE_16_SAMPLES    = 16,
+  D3DMULTISAMPLE_FORCE_DWORD   = 0xffffffff
+} D3DMULTISAMPLE_TYPE, *LPD3DMULTISAMPLE_TYPE;
+
+typedef struct D3DSURFACE_DESC {
+  D3DFORMAT           Format;
+  D3DRESOURCETYPE     Type;
+  DWORD               Usage;
+  D3DPOOL             Pool;
+  D3DMULTISAMPLE_TYPE MultiSampleType;
+  DWORD               MultiSampleQuality;
+  UINT                Width;
+  UINT                Height;
+} D3DSURFACE_DESC, *LPD3DSURFACE_DESC;
 
 #endif // TYPES
