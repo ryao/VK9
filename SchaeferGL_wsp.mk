65c06390df12310506a5bf0c15dea43962e11940
Christopher Joseph Dean Schaefer <disks86@gmail.com>
2014-08-30 21:47:30 -0400

    Changing structure.

--- SchaeferGL_wsp.mk
+++
@@ -1,8 +0,0 @@
-.PHONY: clean All
-
-All:
-	@echo ----------Building project:[ libschaefergl - Debug ]----------
-	@cd "libschaefergl" && "$(MAKE)" -f "libschaefergl.mk"
-clean:
-	@echo ----------Cleaning project:[ libschaefergl - Debug ]----------
-	@cd "libschaefergl" && "$(MAKE)" -f "libschaefergl.mk" clean
