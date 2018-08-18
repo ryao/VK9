169993550764cee1c8c930305214521803d287bf
Max Maisel <max.maisel@posteo.de>
2018-08-18 09:39:48 +0200

    package-release: Allow override exported variables with env.

--- dep32/boost.sh
+++ dep32/boost.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-export BOOST_INCLUDEDIR=/var/opt/boost_1_67/include
-export BOOST_LIBRARYDIR=/var/opt/i686-w64-mingw32/lib
+if [ -z $(export -p | grep BOOST_INCLUDEDIR\=) ]; then
+  export BOOST_INCLUDEDIR=/var/opt/boost_1_67/include
+fi
+if [ -z $(export -p | grep BOOST_LIBRARYDIR\=) ]; then
+  export BOOST_LIBRARYDIR=/var/opt/i686-w64-mingw32/lib
+fi
 
